172d24d9a1eaa2f72046f26677ccb306d2492f27
hidrodat <hidrodat2002@gmail.com>
2022-07-29 13:57:55 +0700

    Initial commit.

---
+++ src/mintNFT_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { mintNFT_backend } from "../../declarations/mintNFT_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await mintNFT_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
